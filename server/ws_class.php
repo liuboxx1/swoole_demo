6dbbff2fc01d0e2c04a9cb0cd5ed91c10d1a3980
liuboxx1@163.com <liuboxx1@163.com>
2018-08-30 16:30:25 +0800

    websocket

--- server/ws_class.php
+++ server/ws_class.php
@@ -26,7 +26,7 @@ class Ws {
     public function onMessage($server, $frame)
     {
         echo "receive from {$frame->fd} : {$frame->data}, opcode : {$frame->opcode}, fin : {$frame->finish} \n";
-        $server->push($frame->fd, "服务器推送数据到客户端");
+        $server->push($frame->fd, $frame->data);
     }
 
     public function onClose($server, $fd)
